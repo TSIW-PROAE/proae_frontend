b4ed261e02e0c6719a8b4617ba78046cf4061a34
eibatatao <allan.thales@ufba.br>
2025-04-04 11:42:23 -0300

    feat: Initial UI with new styles and process selection functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
+import { useState } from "react";
+import "./App.css";
 
-import pgcompLogo from './assets/204805776.png'
-
-import './App.css'
 
 function App() {
+  const processos = [
+    {
+      titulo: "Seleção para benefícios PROAE - Campus Camaçari",
+      status: "Inscrições abertas",
+      cor: "green",
+    },
+    {
+      titulo: "Seleção para benefícios PROAE - Campus Salvador",
+      status: "Inscrições Encerradas",
+      cor: "blue",
+    },
+    {
+      titulo: "RENOVAÇÃO DOS BENEFÍCIOS DA PROAE 2025",
+      status: "Inscrições Encerradas",
+      cor: "blue",
+    },
+    {
+      titulo: "Seleção para benefícios PROAE - Campus Vitória",
+      status: "Inscrições abertas",
+      cor: "green",
+    },
+  ];
+
+  const [expandido, setExpandido] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://github.com/TSIW-PROAE" target="_blank">
-          <img src={pgcompLogo} className="logo react" alt="Pgcomp logo" />
-        </a>
+    <div className="container">
+      <h1 className="titulo">processos seletivos</h1>
+      <div className="botoes-filtro">
+        <button className="filtro">Todos</button>
+        <button className="filtro">Inscrições abertas</button>
+        <button className="filtro">Concluídos</button>
+        <button className="filtro">Editais abertos</button>
       </div>
-      <h1>Bem-vindo ao Sistema PROAE</h1>
-      <div className="card">
-        <p>Estamos em construção...</p>
-      </div>
-    </>
-  )
+      {processos.map((processo, index) => (
+        <div
+          key={index}
+          className={`card ${expandido === index ? "expandido" : ""}`}
+          onClick={() => setExpandido(expandido === index ? null : index)}
+        >
+          <div className="card-header">
+            <h2>{processo.titulo}</h2>
+            <span className={`status ${processo.cor}`}>{processo.status}</span>
+          </div>
+          <p>{processo.edital}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
