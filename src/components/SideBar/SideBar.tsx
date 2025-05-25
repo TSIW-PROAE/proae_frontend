f7ceb7fbc3014239f182e3c8be641c4fd2db759c
macedo01 <pedromacedo113@hotmail.com>
2025-05-25 20:33:37 -0300

    feat: Added logout icon and fixed side bar css

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -9,13 +9,16 @@ import settingsNotSelectedIcon from "../../assets/settings-not-selected.svg";
 import settingsSelectedIcon from "../../assets/settings-selected.svg";
 import docsNotSelectedIcon from "../../assets/docs-not-selected.svg";
 import docsSelectedIcon from "../../assets/docs-selected.svg";
+import logoutIcon from "../../assets/logout-svgrepo-com 1.svg"
 
 export interface ISideBarProps {
     shouldShowDocsIcon?: boolean,
     homeIconRedirect: string,
     processIconRedirect: string,
     configIconRedirect: string,
-    docsIconRedirect?: string
+    docsIconRedirect?: string,
+    logoutIconRedirect: string,
+    logoutOnClick: () => void,
 }
 
 export interface ISideBarIconsConfig {
@@ -24,11 +27,11 @@ export interface ISideBarIconsConfig {
     alt: string,
     redirect: string,
     onClick: () => void,
-    isIconSelected: boolean,
+    isIconSelected?: boolean,
     shouldShowIcon: boolean
 }
 
-const SideBar = ({ homeIconRedirect, processIconRedirect, configIconRedirect, shouldShowDocsIcon = false, docsIconRedirect = ""}: ISideBarProps) => {
+const SideBar = ({ homeIconRedirect, processIconRedirect, configIconRedirect, logoutIconRedirect, logoutOnClick, shouldShowDocsIcon = false, docsIconRedirect = ""}: ISideBarProps) => {
     const navigate = useNavigate();
     const [isHomeSelected, setIsHomeSelected] = useState(true);
     const [isProcessSelected, setIsProcessSelected] = useState(false);
@@ -40,6 +43,7 @@ const SideBar = ({ homeIconRedirect, processIconRedirect, configIconRedirect, sh
         {srcNotSelectedIcon: appsProcessNotSelectedIcon, srcSelectedIcon: appsProcessSelectedIcon, alt: 'process icon', redirect: processIconRedirect, onClick: () => setIsProcessSelected((isProcessSelected) => !isProcessSelected), isIconSelected: isProcessSelected, shouldShowIcon: true},
         {srcNotSelectedIcon: settingsNotSelectedIcon, srcSelectedIcon: settingsSelectedIcon, alt: 'settings icon', redirect: configIconRedirect, onClick: () => setIsConfigSelected((isConfigSelected) => !isConfigSelected), isIconSelected: isConfigSelected, shouldShowIcon: true},
         {srcNotSelectedIcon: docsNotSelectedIcon, srcSelectedIcon: docsSelectedIcon, alt: 'docs icon', redirect: docsIconRedirect, onClick: () => setIsDocsSelected((isDocsSelected) => !isDocsSelected), isIconSelected: isDocsSelected, shouldShowIcon: shouldShowDocsIcon},
+        {srcNotSelectedIcon: logoutIcon, srcSelectedIcon: logoutIcon, alt: 'logout icon', redirect: logoutIconRedirect, onClick: logoutOnClick, shouldShowIcon: true}
     ];
 
     function resetSelection() {
@@ -58,7 +62,7 @@ const SideBar = ({ homeIconRedirect, processIconRedirect, configIconRedirect, sh
                     </div>
                     {icons.map((iconConfig) => (
                         iconConfig.shouldShowIcon && (
-                                    <div id="iconWrapper" style={iconConfig.isIconSelected ? {'border': '2px solid #27548A'} : {}}>
+                                    <div key={iconConfig.srcSelectedIcon} id="iconWrapper" style={iconConfig.isIconSelected ? {'border': '2px solid #27548A'} : {}}>
                                         <img src={iconConfig.isIconSelected ? iconConfig.srcSelectedIcon : iconConfig.srcNotSelectedIcon} alt={iconConfig.alt} key={iconConfig.alt} onClick={() => {navigate(iconConfig.redirect); resetSelection(); iconConfig.onClick();}}/>
                                     </div>
                             )
--- src/pages/DesignSystem/DesignSystem.tsx
+++ src/pages/DesignSystem/DesignSystem.tsx
@@ -5,9 +5,15 @@ import SideBar from "@/components/SideBar/SideBar.tsx";
 import Alert from "@/components/Alert/Alert.tsx";
 
 export default function DesignSystem() {
+
+  const handleLogout = () => {
+    console.log("Logging out....")
+  };
+
   return (
     <div>
       <h1>Design System</h1>
+      <SideBar homeIconRedirect={""} processIconRedirect={""} configIconRedirect={""} logoutIconRedirect={"/"} logoutOnClick={handleLogout} shouldShowDocsIcon={true} docsIconRedirect={""}/>
       <div className="component-section">
         <h2>Botões</h2>
         <div className="component-row">
@@ -18,7 +24,6 @@ export default function DesignSystem() {
           <Button color="danger">Perigo</Button>
         </div>
           <CardUserComponent aluno={{"nome": "Allan", "matricula": "2000222", "dataCadastro": "20/12/2200", "validade": "20/12/12", "status": "de boas", "foto": "", beneficios: [{"nome": "beneficio1", "edital": "sla", "validade": "20/12/2024", "status": "ativo"}]}}/>
-          <SideBar homeIconRedirect={""} processIconRedirect={""} configIconRedirect={""} shouldShowDocsIcon={true} docsIconRedirect={""}/>
           <Alert titulo='Inscrição Confirmada' descricao='Inscrição realizada.' data='03/04'/>
           <Alert titulo='Documentação Negada' descricao='Caro aluno, por favor faça o reenvio do documento de matricula devidamente atualizado, conforme o especificado no item 2.3 do edital.' data='04/04'/>
           <Alert titulo='Documentação Pendente' descricao='Caro aluno, por favor faça o envio do CAD Único.' data='04/04'/>
