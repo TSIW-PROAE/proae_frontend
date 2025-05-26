ba0eb7df8518361c78a2eb2cf32317eaeb000626
Hevelin de Jesus Freitas <55918680+HevelinFR@users.noreply.github.com>
2025-05-26 13:19:29 -0300

    Integração (#19)
    * cadastro-integracao
    
    * clerk - parte 1
    
    * feat: Adiciona integração do cadastro de aluno com serviço e configura variáveis de ambiente
    
    * Correção Toast de erro login
    
    * feat: Atualiza rotas de autenticação e navegação para o portal do aluno
    
    ---------
    
    Co-authored-by: Caio Alcarria Martins dos Santos <alcarriacaio@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import { Provider } from "./provider.tsx";
 import "@/styles/variables.css";
@@ -8,10 +9,49 @@ import "@/styles/globals.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
+const PUBLISHABLE_KEY =
+  "pk_test_aWRlYWwtbWlkZ2UtNjkuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
-      <RouterProvider router={router} />
-    </Provider>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: undefined,
+        layout: {
+          socialButtonsVariant: "iconButton",
+          logoPlacement: "inside",
+          showOptionalFields: true,
+        },
+        variables: {
+          colorPrimary: "#0070f3",
+          colorText: "#000000",
+          colorBackground: "#ffffff",
+        },
+      }}
+      options={{
+        allowedRedirectOrigins: [
+          "http://localhost:5173",
+          "http://localhost:3000",
+          "http://127.0.0.1:5173",
+          "http://127.0.0.1:3000",
+        ],
+        signInUrl: "/login-aluno",
+        signUpUrl: "/cadastro-aluno",
+        afterSignInUrl: "/dashboard-aluno",
+        afterSignUpUrl: "/dashboard-aluno",
+        redirectUrl: "/dashboard-aluno",
+        routing: "path",
+        path: "/",
+        basePath: "/",
+        frontendApi: "https://ideal-midge-69.clerk.accounts.dev",
+        apiUrl: "https://api.clerk.dev",
+        apiVersion: "v1",
+      }}
+    >
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
+    </ClerkProvider>
   </React.StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -45,7 +45,10 @@ const routes = [
           { path: "portal-aluno/documentacao", element: <DocumentacaoAluno /> },
           { path: "portal-aluno/configuracao", element: <ConfiguracaoAluno /> },
           { path: "portal-aluno/candidatura", element: <CandidaturaAluno /> },
-          { path: "portal-aluno/pendencias-aluno", element: <PendenciasAluno /> },
+          {
+            path: "portal-aluno/pendencias-aluno",
+            element: <PendenciasAluno />,
+          },
         ],
       },
       { path: "login-proae", element: <LoginProae /> },
