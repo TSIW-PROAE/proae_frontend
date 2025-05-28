231227425c46c7081f4142b119ed4d609cc981d2
Saulo Sampaio <saulo.samb2@gmail.com>
2025-05-28 08:50:42 -0300

    fix: consertar a pagina de configuracao para a nova sidebar
    Removi a chamada manual para a sidebar na página de configuração. Agora a página utiliza a sidebar atualizada.

--- src/pages/paginaAluno/ConfiguracaoAluno/ConfiguracaoAluno.tsx
+++ src/pages/paginaAluno/ConfiguracaoAluno/ConfiguracaoAluno.tsx
@@ -1,22 +1,12 @@
-import SideBar from "@/components/Side Bar/SideBar";
 import "./ConfiguracaoAluno.css";
 import AlunoForm from "../../../components/FormularioConfiguracao/Form";
 
 export default function ConfiguracaoAluno() {
   return (
     <div className="container">
-      <div id="side-bar">
-        <SideBar
-          homeIconRedirect="/portal-aluno"
-          processIconRedirect="/portal-aluno/processos"
-          configIconRedirect="/portal-aluno/configuracao"
-          shouldShowDocsIcon={false}
-          defaultSelected="config"
-        />
-      </div>
       <div id="page-config-wrapper">
         <div id="title">
-          <h1>Configurações do Aluno</h1>
+          <h1>Perfil do Aluno</h1>
         </div>
         <div className="form">
             <AlunoForm />
