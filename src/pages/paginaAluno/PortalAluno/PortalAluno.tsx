2c0098ff38aabfd6f8adee9c687bb695f461c377
Gustavo Santos <gustavosisa@outlook.com>
2025-06-09 10:11:48 -0300

    chore: remove non used imports

--- src/pages/paginaAluno/PortalAluno/PortalAluno.tsx
+++ src/pages/paginaAluno/PortalAluno/PortalAluno.tsx
@@ -1,13 +1,13 @@
 // PortalAluno.tsx
-import PageLayout from "@/pages/PageLayout/PageLayout";
 import BenefitsCard from "@/components/BenefitsCard/BenefitsCard";
+import PageLayout from "@/pages/PageLayout/PageLayout";
 import OpenSelections from "@/pages/paginaAluno/PortalAluno/componentes/OpenSelections";
-import "./PortalAluno.css";
-import CandidateStatus from "./componentes/CandidateStatus";
-import { useClerk } from "@clerk/clerk-react";
 import { FetchAdapter } from "@/services/BaseRequestService/HttpClient";
 import PortalAlunoService from "@/services/PortalAluno/PortalAlunoService";
-import { useEffect, useState } from 'react'
+import { useClerk } from "@clerk/clerk-react";
+import { useEffect, useState } from 'react';
+import "./PortalAluno.css";
+import CandidateStatus from "./componentes/CandidateStatus";
 
 
 interface ResponseData {
@@ -69,7 +69,6 @@ export default function PortalAluno() {
       }
   };
 
-
   useEffect(() => {
     if (userId) {
       getBenefits();
@@ -77,106 +76,6 @@ export default function PortalAluno() {
       getInscriptions();
     }
   }, [userId]);
-
-  
-
-  
-
-  const editalsMock = [
-    {
-      id: 1,
-      title: "Auxílio Permanência - Campus Salvador",
-      year: 2025,
-      status: {
-        title: "Aguardando Documentação",
-        status: "pending",
-        color: "yellow", // Apenas a cor
-      },
-      pendingItems: [
-        {
-          name: "RG",
-          reason: "Imagem ilegível",
-          requestDate: "2025-05-09T10:15:00Z",
-        },
-        {
-          name: "CAD Único",
-          reason: "Não enviado",
-          requestDate: "2025-05-09T10:16:00Z",
-        },
-        {
-          name: "Comprovante de Residência",
-          reason: "Documento vencido",
-          requestDate: "2025-05-10T08:00:00Z",
-        },
-        {
-          name: "Histórico Escolar",
-          reason: "Faltando assinatura",
-          requestDate: "2025-05-10T08:10:00Z",
-        },
-        {
-          name: "Declaração de Matrícula",
-          reason: "Formato inválido",
-          requestDate: "2025-05-10T08:20:00Z",
-        },
-        {
-          name: "Foto 3x4",
-          reason: "Não enviado",
-          requestDate: "2025-05-10T08:30:00Z",
-        },
-        {
-          name: "Título de Eleitor",
-          reason: "Imagem cortada",
-          requestDate: "2025-05-10T08:40:00Z",
-        },
-        {
-          name: "Comprovante de Renda",
-          reason: "Informação incompatível",
-          requestDate: "2025-05-10T08:50:00Z",
-        },
-        {
-          name: "CPF",
-          reason: "Documento ilegível",
-          requestDate: "2025-05-10T09:00:00Z",
-        },
-        {
-          name: "Declaração de Imposto de Renda",
-          reason: "Faltando página",
-          requestDate: "2025-05-10T09:10:00Z",
-        },
-      ],
-      stages: [
-        "Inscrição",
-        "Homologação das Inscrições",
-        "Análise de Documentos",
-        "Resultado Preliminar",
-        "Interposição do Resultado Parcial",
-        "Resultado do Recurso",
-        "Resultado Final",
-      ],
-      currentStageIndex: 2,
-    },
-    {
-      id: 2,
-      title: "Auxílio Moradia - Campus Salvador",
-      year: 2025,
-      status: {
-        title: "Aprovado",
-        status: "approved",
-        color: "green",
-      },
-      pendingItems: [],
-      stages: [
-        "Inscrição",
-        "Homologação das Inscrições",
-        "Interposição das Inscrições",
-        "Resultado Preliminar",
-        "Análise de Documentos",
-        "Resultado do Recurso",
-        "Resultado Final",
-      ],
-      currentStageIndex: 6,
-    },
-  ];
 
   return (
     <PageLayout>
