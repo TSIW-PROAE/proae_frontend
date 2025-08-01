a957b2b1f6336926400ea08db837aa6dde89c117
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-08-01 15:08:23 -0300

    reafactor(CadastroAluno): Tirando dados estáticos da componente aluno e trazendo ele para pasta utils

---
+++ src/utils/cadastroop.ts
@@ -0,0 +1,220 @@
+  export const pronomes = [
+    { valor: "ele/dele", label: "Ele/Dele" },
+    { valor: "ela/dela", label: "Ela/Dela" },
+    { valor: "elu/delu", label: "Elu/Delu" },
+    { valor: "Prefiro não informar", label: "Prefiro não informar" },
+  ];
+
+  export const cursos = [
+    { valor: "Arquitetura e Urbanismo", label: "Arquitetura e Urbanismo" },
+    {
+      valor: "Arquitetura e Urbanismo – Noturno",
+      label: "Arquitetura e Urbanismo – Noturno",
+    },
+    { valor: "Engenharia da Computação", label: "Engenharia da Computação" },
+    {
+      valor: "Engenharia de Agrimensura e Cartográfica",
+      label: "Engenharia de Agrimensura e Cartográfica",
+    },
+    {
+      valor: "Engenharia de Controle e Automação",
+      label: "Engenharia de Controle e Automação",
+    },
+    { valor: "Engenharia de Minas", label: "Engenharia de Minas" },
+    { valor: "Engenharia de Produção", label: "Engenharia de Produção" },
+    { valor: "Engenharia Elétrica", label: "Engenharia Elétrica" },
+    { valor: "Engenharia Mecânica", label: "Engenharia Mecânica" },
+    { valor: "Engenharia Química", label: "Engenharia Química" },
+    {
+      valor: "Ciências Sociais (Lic. e Bach.)",
+      label: "Ciências Sociais (Lic. e Bach.)",
+    },
+    { valor: "Filosofia", label: "Filosofia" },
+    { valor: "História (Lic. e Bach.)", label: "História (Lic. e Bach.)" },
+    { valor: "História (Lic.) – Noturno", label: "História (Lic.) – Noturno" },
+    { valor: "Museologia", label: "Museologia" },
+    {
+      valor: "Psicologia – Formação de Psicólogo",
+      label: "Psicologia – Formação de Psicólogo",
+    },
+    { valor: "Serviço social", label: "Serviço social" },
+    {
+      valor: "Tecnologia em Transporte Terrestre",
+      label: "Tecnologia em Transporte Terrestre",
+    },
+    { valor: "Ciência da Computação", label: "Ciência da Computação" },
+    { valor: "Estatística", label: "Estatística" },
+    { valor: "Física (Lic) – Noturno", label: "Física (Lic) – Noturno" },
+    { valor: "Física (Lic. e Bach.)", label: "Física (Lic. e Bach.)" },
+    { valor: "Geofísica", label: "Geofísica" },
+    {
+      valor: "Geografia (Lic.) – Noturno",
+      label: "Geografia (Lic.) – Noturno",
+    },
+    {
+      valor: "Licenciatura em Computação – Noturno",
+      label: "Licenciatura em Computação – Noturno",
+    },
+    {
+      valor: "Matemática (Lic.) – Noturno",
+      label: "Matemática (Lic.) – Noturno",
+    },
+    { valor: "Oceanografia", label: "Oceanografia" },
+    {
+      valor: "Química (Lic. Bach. e Química Industrial)",
+      label: "Química (Lic. Bach. e Química Industrial)",
+    },
+    { valor: "Química (Lic.)", label: "Química (Lic.)" },
+    {
+      valor: "Sistemas de Informação – Bacharelado",
+      label: "Sistemas de Informação – Bacharelado",
+    },
+    {
+      valor: "Ciências Biológicas (Lic. e Bach.)",
+      label: "Ciências Biológicas (Lic. e Bach.)",
+    },
+    {
+      valor: "Ciências Biológicas (Lic.) – Noturno",
+      label: "Ciências Biológicas (Lic.) – Noturno",
+    },
+    { valor: "Farmácia", label: "Farmácia" },
+    { valor: "Farmácia – Noturno", label: "Farmácia – Noturno" },
+    { valor: "Gastronomia", label: "Gastronomia" },
+    {
+      valor: "Licenciatura em Ciências Naturais",
+      label: "Licenciatura em Ciências Naturais",
+    },
+    { valor: "Medicina Veterinária", label: "Medicina Veterinária" },
+    { valor: "Zootecnia", label: "Zootecnia" },
+    { valor: "Comunicação – Jornalismo", label: "Comunicação – Jornalismo" },
+    {
+      valor: "Comunicação – Produção em Comunicação e Cultura",
+      label: "Comunicação – Produção em Comunicação e Cultura",
+    },
+    {
+      valor: "Estudos de Gênero e Diversidade (Bach.)",
+      label: "Estudos de Gênero e Diversidade (Bach.)",
+    },
+    {
+      valor: "Letras Vernáculas (Lic. e Bach.)",
+      label: "Letras Vernáculas (Lic. e Bach.)",
+    },
+    { valor: "Letras Vernáculas (Lic.)", label: "Letras Vernáculas (Lic.)" },
+    {
+      valor: "Letras Vernáculas e Língua Estrangeira Moderna (Lic.)",
+      label: "Letras Vernáculas e Língua Estrangeira Moderna (Lic.)",
+    },
+    {
+      valor: "Língua Estrangeira – Inglês/Espanhol (Lic.)",
+      label: "Língua Estrangeira – Inglês/Espanhol (Lic.)",
+    },
+    {
+      valor: "Língua Estrangeira Moderna ou Clássica (Lic. e Bach.)",
+      label: "Língua Estrangeira Moderna ou Clássica (Lic. e Bach.)",
+    },
+    { valor: "Dança", label: "Dança" },
+    { valor: "Artes", label: "Artes" },
+    { valor: "Artes – Noturno", label: "Artes – Noturno" },
+    {
+      valor: "Ciência e Tecnologia – Noturno",
+      label: "Ciência e Tecnologia – Noturno",
+    },
+    { valor: "Humanidades – Noturno", label: "Humanidades – Noturno" },
+    { valor: "Saúde", label: "Saúde" },
+    { valor: "Saúde – Noturno", label: "Saúde – Noturno" },
+    { valor: "Enfermagem", label: "Enfermagem" },
+    { valor: "Fisioterapia", label: "Fisioterapia" },
+    { valor: "Fonoaudiologia", label: "Fonoaudiologia" },
+    { valor: "Medicina", label: "Medicina" },
+    { valor: "Nutrição", label: "Nutrição" },
+    { valor: "Odontologia", label: "Odontologia" },
+    { valor: "Saúde Coletiva", label: "Saúde Coletiva" },
+    { valor: "Terapia Ocupacional", label: "Terapia Ocupacional" },
+    { valor: "Arquivologia", label: "Arquivologia" },
+    { valor: "Arquivologia – Noturno", label: "Arquivologia – Noturno" },
+    {
+      valor: "Biblioteconomia e Documentação",
+      label: "Biblioteconomia e Documentação",
+    },
+    { valor: "Direito", label: "Direito" },
+    { valor: "Direito – Noturno", label: "Direito – Noturno" },
+    {
+      valor: "Licenciatura em Educação Física",
+      label: "Licenciatura em Educação Física",
+    },
+    { valor: "Pedagogia", label: "Pedagogia" },
+    { valor: "Secretariado Executivo", label: "Secretariado Executivo" },
+    {
+      valor: "Artes Cênicas – Direção Teatral",
+      label: "Artes Cênicas – Direção Teatral",
+    },
+    {
+      valor: "Artes Cênicas – Interpretação Teatral",
+      label: "Artes Cênicas – Interpretação Teatral",
+    },
+    { valor: "Artes Plásticas", label: "Artes Plásticas" },
+    { valor: "Canto", label: "Canto" },
+    { valor: "Composição e Regência", label: "Composição e Regência" },
+    {
+      valor: "Curso Superior de Decoração",
+      label: "Curso Superior de Decoração",
+    },
+    { valor: "Design", label: "Design" },
+    { valor: "Instrumento", label: "Instrumento" },
+    {
+      valor: "Licenciatura em Desenho e Plástica",
+      label: "Licenciatura em Desenho e Plástica",
+    },
+    { valor: "Licenciatura em Música", label: "Licenciatura em Música" },
+    { valor: "Licenciatura em Teatro", label: "Licenciatura em Teatro" },
+    { valor: "Música Popular", label: "Música Popular" },
+    {
+      valor: "Gestão Pública e Gestão Social",
+      label: "Gestão Pública e Gestão Social",
+    },
+    { valor: "Ciências Contábeis", label: "Ciências Contábeis" },
+    {
+      valor: "Ciências Contábeis – Noturno",
+      label: "Ciências Contábeis – Noturno",
+    },
+    { valor: "Ciências Econômicas", label: "Ciências Econômicas" },
+    { valor: "Engenharia Civil", label: "Engenharia Civil" },
+    {
+      valor: "Engenharia Sanitária e Ambiental",
+      label: "Engenharia Sanitária e Ambiental",
+    },
+    { valor: "Geografia (Lic. e Bach.)", label: "Geografia (Lic. e Bach.)" },
+    { valor: "Geologia", label: "Geologia" },
+    { valor: "Matemática (Lic. e Bach.)", label: "Matemática (Lic. e Bach.)" },
+    { valor: "Química (Lic. e Bach.)", label: "Química (Lic. e Bach.)" },
+    {
+      valor: "Ciências Biológicas (Lic. e Bach.) – Barreiras",
+      label: "Ciências Biológicas (Lic. e Bach.) – Barreiras",
+    },
+    { valor: "Administração", label: "Administração" },
+    { valor: "Ciência e Tecnologia", label: "Ciência e Tecnologia" },
+    { valor: "Humanidades", label: "Humanidades" },
+    { valor: "Biotecnologia", label: "Biotecnologia" },
+    {
+      valor: "Ciências Biológicas (Bach.)",
+      label: "Ciências Biológicas (Bach.)",
+    },
+    {
+      valor: "Enfermagem – Vitória da Conquista",
+      label: "Enfermagem – Vitória da Conquista",
+    },
+    {
+      valor: "Farmácia – Vitória da Conquista",
+      label: "Farmácia – Vitória da Conquista",
+    },
+    {
+      valor: "Nutrição – Vitória da Conquista",
+      label: "Nutrição – Vitória da Conquista",
+    },
+    { valor: "B.I. – C.T.I", label: "B.I. – C.T.I" },
+  ];
+
+  export const campus = [
+    { valor: "Salvador", label: "Salvador" },
+    { valor: "Vitória da Conquista", label: "Vitória da Conquista" },
+  ];
