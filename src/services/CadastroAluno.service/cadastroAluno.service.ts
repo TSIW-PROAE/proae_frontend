c55419bf66adabe0fa95754730f50c27bacac277
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-08-07 23:11:55 -0300

    feat: Novos funções para cadastro e login básico sem uso de Oauth

--- src/services/CadastroAluno.service/cadastroAluno.service.ts
+++ src/services/CadastroAluno.service/cadastroAluno.service.ts
@@ -1,20 +1,31 @@
+import { setCookie } from "@/utils/utils";
 import IHttpClient from "../BaseRequestService/HttpClient";
+import { UserSignup, UserLogin, UserLoginResponse } from "@/types/auth";
 
 export default class CadastroAlunoService {
   headerToken: string;
 
   constructor(private readonly httpClient: IHttpClient) {
-    //TODO: Implementar o token de autenticação
-    // this.headerToken = localStorage.getItem('token') || '';
     this.headerToken = "";
   }
 
-  async createAlunoUser(data: object) {
-    //console.log("first");
-    //console.log(import.meta.env.VITE_API_URL_SERVICES);
+  async createAlunoUser(data: UserSignup) {
     const url = import.meta.env.VITE_API_URL_SERVICES + `/auth/signup`;
     const response = await this.httpClient.post(url, data, this.headerToken);
     return response;
   }
-  
+
+  async LoginAluno(data: UserLogin){
+    const url = import.meta.env.VITE_API_URL_SERVICES + `/auth/login`;
+    const response: UserLoginResponse = await this.httpClient.post(url, data, this.headerToken);
+    setCookie("token", response.acess_token, 7);
+    return response;
+  }
+
+  async validateToken() {
+    const url = import.meta.env.VITE_API_URL_SERVICES + `/auth/validate`;
+    const response: UserLoginResponse = await this.httpClient.get(url, this.headerToken);
+    return response;
+  }
+
 }
