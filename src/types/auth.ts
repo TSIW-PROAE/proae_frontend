91bc04d64d4a6d8755dde90acfc10befdb74b794
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-08-07 23:07:45 -0300

    feat: mais tipos para validar retornos do backend

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -3,16 +3,15 @@ export interface UserInfo{
   id: number;
   nome: string;
   email: string;
-  role: "aluno" | "proae";
   [key: string]: any;
 }
 
 export interface AuthContextType{
   isAuthenticated: boolean;
   userInfo: UserInfo | null;
-  login: () => void;
+  login: (data: UserLogin) => void;
   logout: () => void;
-  register: () => void;
+  register: (data: UserSignup) => void;
   Oauth_login: () => void;
   Oauth_logout: () => void;
   Oauth_register: () => void;
@@ -37,6 +36,15 @@ export type UserSignup = {
 export type UserLogin = {
   email: string;
   senha: string;
+}
+
+export interface UserLoginResponse {
+  acess_token: string;
+  user: {
+    id: number;
+    nome: string;
+    email: string;
+  };
 }
 
 // ideia de criar um interface para validar o signup do usuário
