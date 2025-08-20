a6fb88b129178f3bc1a5eb857a2fb935386ddf27
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-08-20 13:58:45 -0300

    PU-57 feat: Adicionando validações usando o react-hook-form e mudando a abordagem do hooj de validação

--- src/pages/paginaAluno/resetPassword/ResetPassoword.tsx
+++ src/pages/paginaAluno/resetPassword/ResetPassoword.tsx
@@ -1,4 +1,4 @@
-import { redirect, useSearchParams } from "react-router-dom"
+import {  useSearchParams } from "react-router-dom"
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import { FetchAdapter } from "@/services/BaseRequestService/HttpClient";
 import CadastroAlunoService from "@/services/CadastroAluno.service/cadastroAluno.service";
@@ -6,15 +6,13 @@ import { toast, Toaster } from "react-hot-toast";
 import {Input} from "@heroui/input"
 import "./ResetPassword.css"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IResetPassword } from "@/types/auth";
 
-interface IResetPassword {
-  newPassword: string;
-  confirmPassword: string;
-}
 
 export default function ResetPassword() {
   const [isLoading, setIsLoading] = useState(false);
-  const { handleSubmit, control } = useForm<IResetPassword>({
+  const { handleSubmit, control, formState: { errors }, setError, watch } = useForm<IResetPassword>({
     defaultValues: {
       newPassword: "",
       confirmPassword: ""
@@ -22,36 +20,54 @@ export default function ResetPassword() {
   });
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token")
+  const navigate = useNavigate();
+  const newPassword = watch("newPassword");
 
   const handleOnSubmit: SubmitHandler<IResetPassword> = async (data) => {
     setIsLoading(true);
+    let hasErrors = false;
 
     const client = new FetchAdapter();
     const cadastroAlunoService = new CadastroAlunoService(client);
 
     try {
+      if(data.newPassword.length < 8) {
+        setError("newPassword", {
+          type: "minLength",
+          message: "A nova senha deve ter pelo menos 8 caracteres."
+        });
+        hasErrors = true;
+      }
+
       if (data.newPassword !== data.confirmPassword) {
-        toast.error("As senhas não coincidem.");
+        setError("confirmPassword", {
+          type: "mismatch",
+          message: "As senhas não coincidem."
+        });
+        hasErrors = true;
+      }
+
+      if (hasErrors) {
+        setIsLoading(false);
         return;
       }
 
       if(!token) {
         toast.error("Token inválido ou ausente.");
+        setIsLoading(false);
         return;
       }
 
-      if(data.newPassword.length < 6) {
-        toast.error("A senha deve ter pelo menos 6 caracteres.");
-        return;
-      }
-
-      await cadastroAlunoService.resetPassword(token, data.newPassword);
-      setIsLoading(false);
+      await cadastroAlunoService.resetPassword({...data, token});
       toast.success("Senha alterada com sucesso!");
-      redirect("/login-aluno");
+      navigate("/login-aluno")
     } catch (error) {
-      setIsLoading(false);
-      toast.error("Erro ao alterar a senha. Por favor, tente novamente.");
+      if((error as any)?.statusCode == 401){
+        toast.error("Erro ao alterar a senha. Por favor, tente novamente mais tarde.");
+         navigate("/")
+      } else {
+        toast.error("Erro ao alterar a senha. Tente novamente.");
+      }
     } finally{
       setIsLoading(false);
     }
@@ -70,6 +86,13 @@ export default function ResetPassword() {
           <Controller
             name="newPassword"
             control={control}
+            rules={{
+              required: "Nova senha é obrigatória",
+              minLength: {
+                value: 8,
+                message: "A nova senha deve ter pelo menos 8 caracteres"
+              }
+            }}
             render={({field}) => (
               <Input
                 {...field}
@@ -81,10 +104,11 @@ export default function ResetPassword() {
                 placeholder="Digite sua nova senha"
                 fullWidth
                 disabled={isLoading}
+                isInvalid={!!errors.newPassword}
+                errorMessage={errors.newPassword?.message}
                 classNames={{
                   base: "custom-input",
                 }}
-                required
               />
             )}
           />
@@ -92,6 +116,15 @@ export default function ResetPassword() {
           <Controller
             name="confirmPassword"
             control={control}
+            rules={{
+              required: "Confirmação de senha é obrigatória",
+              validate: (value) => {
+                if (value !== newPassword) {
+                  return "As senhas não coincidem";
+                }
+                return true;
+              }
+            }}
             render={({field}) => (
               <Input
                 {...field}
@@ -103,10 +136,11 @@ export default function ResetPassword() {
                 placeholder="Confirme sua nova senha"
                 fullWidth
                 disabled={isLoading}
+                isInvalid={!!errors.confirmPassword}
+                errorMessage={errors.confirmPassword?.message}
                 classNames={{
                   base: "custom-input",
                 }}
-                required
               />
             )}
           />
