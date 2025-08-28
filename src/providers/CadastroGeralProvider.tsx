b41772138e27616802572d9e4c6b65013bc2d499
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-08-28 14:05:16 -0300

    PU-61 fix: Adicionando validação para identificar o questionario de cadastro geral

--- src/providers/CadastroGeralProvider.tsx
+++ src/providers/CadastroGeralProvider.tsx
@@ -2,23 +2,21 @@ import { CadastroGeralContext } from "@/context/CadastroGeralContext";
 import { AuthContext } from "@/context/AuthContext";
 import { useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import toast from "react-hot-toast";
+import toast, {Toaster} from "react-hot-toast";
 
 
 export default function CadastroGeralProvider({ children }:{children: React.ReactNode}) {
   const {userInfo} = useContext(AuthContext)
-  const hasCg = userInfo?.hasCg || false;;
+  const hasCg = userInfo?.hasCg || false;
   const navigate = useNavigate();
   const location = useLocation();
 
   if(!hasCg){
-    if(location.pathname.includes('questionario')){
-      toast.error("Para Acessar o Este questionário você precisa ter o cadastro geral ativo");
-      navigate('/cadastro');
+    if(location.pathname.includes('questionario') && !location.pathname.includes('cadastroGeral')){
+      toast.error("Para Acessar o Este questionário você precisa ter o cadastro geral ativo!");
+      navigate('');
     }
   }
-
-
 
   return (
     <CadastroGeralContext.Provider value={{ isValidCg: hasCg }}>
