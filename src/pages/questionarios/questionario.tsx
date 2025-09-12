3c62fc3fe1020d402f464eaf51faf1598b3a8b46
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-09-12 09:30:32 -0300

    feat: Integrar EditalService no componente Questionario para buscar dados do edital

--- src/pages/questionarios/questionario.tsx
+++ src/pages/questionarios/questionario.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormularioDinamico } from '../../components/FormularioDinamico/FormularioDinamico';
 import { useParams } from 'react-router-dom';
+import {EditalService} from "@/services/EditalService/editalService"
 
 export const Questionario: React.FC = () => {
   const { editalId } = useParams<{ editalId: string }>();
+  const [editalData, setEditalData] = useState<any>(null);
+
+  const editalService = new EditalService();
 
   const handleSuccess = (data: any) => {
     console.log("Dados do formulário enviados com sucesso:", data);
@@ -13,11 +17,20 @@ export const Questionario: React.FC = () => {
     console.error("Erro ao enviar formulário:", error);
   }
 
+  useEffect(() => {
+    const edital = async () => {
+      const response = await editalService.buscarEditalPorId(Number(editalId));
+      setEditalData(response);
+    }
+
+    edital();
+  }, [editalId])
+
   return (
     <FormularioDinamico
       editalId={Number(editalId)}
-      titulo="Formulário de Cadastro Socioeconômico - PROAE"
-      subtitulo="Preencha todas as informações para análise da sua situação socioeconômica"
+      titulo={`Formulário ${editalData ? editalData.titulo_edital : ""}`}
+      subtitulo={`${editalData ? editalData.descricao : ""}`}
       botaoFinal="Enviar Formulário"
       onSuccess={handleSuccess}
       onError={handleError}
