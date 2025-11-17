aa977e7716bd406e571b76039aa287a207e3f878
jaedsonmacedo <jaedsonmacedo@infojr.com.br>
2025-11-17 07:21:10 -0300

    refactor: mudando estilos para o tailwindcss e adicionando campo para volta a tela de login

--- src/pages/paginaAluno/CadastroAluno/CadastroAluno.tsx
+++ src/pages/paginaAluno/CadastroAluno/CadastroAluno.tsx
@@ -6,7 +6,6 @@ import { DatePicker } from "@heroui/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { DateValue } from "@internationalized/date";
 import { toast, Toaster } from "react-hot-toast";
-import "./CadastroAluno.css";
 import AuthService from "../../../services/AuthService/auth.service";
 import useFormValidation, { FormData } from "@/hooks/useFormValidation";
 import { formatarData, formatarCelular, formatCPF } from "../../../utils/validations";
@@ -28,7 +27,6 @@ export default function Cadastro() {
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const anoAtual = new Date().getFullYear();
   const location = useLocation();
   const navigate = useNavigate();
   const { validateForm, errors, setFieldError } = useFormValidation(formData);
@@ -123,240 +121,178 @@ export default function Cadastro() {
   };
 
   return (
-    <div id="cadastro" className="cadastro-container">
+    <section className="w-full min-h-screen flex items-center justify-center bg-[#183b4e] py-6 px-4">
       <Toaster position="top-right" />
-      <div className="cadastro-content">
-        {/* <div className="cadastro-image-container">
-            <img
-                src="src/assets/logo-ufba-PROAE.png"
-                alt="Logo UFBA"
-                className="cadastro-image"
-            />
-        </div> */}
+      
+      <div className="w-full max-w-2xl bg-white rounded-lg shadow-md p-8">
+        
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+          Cadastre-se!
+        </h2>
 
-        <div className="cadastro-form">
-          <a
-            href="/"
-            className="voltar-link-cadastro"
-            style={{ color: "var(--cor-creme)" }}
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <Input
+            id="nome"
+            value={formData.nome}
+            label="Nome"
+            variant="bordered"
+            type="text"
+            placeholder="Digite o seu nome"
+            onChange={(e) => handleInputChange("nome", e.target.value)}
+            isInvalid={!!errors.nome}
+            errorMessage={errors.nome}
+            fullWidth
+          />
+
+          
+          <DatePicker
+            label="Data de Nascimento"
+            variant="bordered"
+            fullWidth
+            value={formData.dataNascimento}
+            onChange={(date) => handleInputChange("dataNascimento", date)}
+            isInvalid={!!errors.dataNascimento}
+            errorMessage={errors.dataNascimento}
+          />
+
+          <Input
+            type="text"
+            label="Curso"
+            variant="bordered"
+            fullWidth
+            placeholder="Digite seu curso"
+            onChange={(e) => handleInputChange("curso", e.target.value)}
+            isInvalid={!!errors.curso}
+            errorMessage={errors.curso}
+          />
+
+          <Select
+            label="Campus"
+            variant="bordered"
+            fullWidth
+            selectedKeys={[formData.campus]}
+            onChange={(e) => handleInputChange("campus", e.target.value)}
+            isInvalid={!!errors.campus}
+            errorMessage={errors.campus}
           >
-            <span style={{ marginRight: "5px" }}>←</span>
-            Página inicial
+            {campus.map((campi) => (
+              <SelectItem key={campi.valor}>{campi.label}</SelectItem>
+            ))}
+          </Select>
+
+          <Input
+            id="cpf"
+            value={formData.cpf}
+            label="CPF"
+            variant="bordered"
+            type="text"
+            placeholder="Digite seu CPF"
+            onChange={(e) =>
+              handleInputChange("cpf", formatCPF(e.target.value))
+            }
+            isInvalid={!!errors.cpf}
+            errorMessage={errors.cpf}
+            fullWidth
+          />
+
+          <DatePicker
+            label="Data de Ingresso"
+            variant="bordered"
+            fullWidth
+            value={formData.dataIngresso}
+            onChange={(date) => handleInputChange("dataIngresso", date)}
+            isInvalid={!!errors.dataIngresso}
+            errorMessage={errors.dataIngresso}
+          />
+
+          <Input
+            id="matricula"
+            value={formData.matricula}
+            label="Matrícula"
+            variant="bordered"
+            type="text"
+            placeholder="Digite a sua matrícula"
+            onChange={(e) =>
+              handleInputChange("matricula", e.target.value)
+            }
+            isInvalid={!!errors.matricula}
+            errorMessage={errors.matricula}
+            fullWidth
+          />
+
+          <Input
+            id="email"
+            value={formData.email}
+            label="Email"
+            variant="bordered"
+            type="email"
+            placeholder="Digite seu email @ufba.br"
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            isInvalid={!!errors.email}
+            errorMessage={errors.email}
+            fullWidth
+          />
+
+          <Input
+            id="celular"
+            value={formData.celular}
+            label="Celular"
+            variant="bordered"
+            type="text"
+            placeholder="Digite o seu celular"
+            onChange={(e) => handleInputChange("celular", e.target.value)}
+            isInvalid={!!errors.celular}
+            errorMessage={errors.celular}
+            fullWidth
+          />
+
+          <Input
+            id="senha"
+            label="Senha"
+            variant="bordered"
+            type="password"
+            placeholder="Digite sua senha"
+            value={formData.senha}
+            onChange={(e) => handleInputChange("senha", e.target.value)}
+            isInvalid={!!errors.senha}
+            errorMessage={errors.senha}
+            fullWidth
+          />
+
+          <Input
+            id="confirmarSenha"
+            label="Repita sua senha"
+            variant="bordered"
+            type="password"
+            placeholder="Digite sua senha novamente"
+            value={formData.confirmarSenha}
+            onChange={(e) =>
+              handleInputChange("confirmarSenha", e.target.value)
+            }
+            isInvalid={!!errors.confirmarSenha}
+            errorMessage={errors.confirmarSenha}
+            fullWidth
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            className="bg-[#183b4e] hover:bg-[#14526d] text-white p-6 rounded-xl font-semibold text-base mt-2"
+            isLoading={isSubmitting}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Cadastrando..." : "Cadastrar"}
+          </Button>
+
+          <a 
+            href="/login" 
+            className="text-[#183b4e] underline text-center text-sm md:text-base"
+          >
+            Já possui uma conta? Faça login
           </a>
 
-          <div className="form-container">
-            <div className="cadastro-header">
-              <h2 className="cadastro-title">Cadastre-se!</h2>
-            </div>
-
-            <form className="cadastro-form-container" onSubmit={handleSubmit}>
-              <div className="input-nomes">
-                <div className="input-container">
-                  <Input
-                    id="nome"
-                    value={formData.nome}
-                    label="Nome"
-                    variant="bordered"
-                    radius="lg"
-                    type="text"
-                    placeholder="Digite o seu nome"
-                    onChange={(e) => handleInputChange("nome", e.target.value)}
-                    isInvalid={!!errors.nome}
-                    errorMessage={errors.nome}
-                    fullWidth
-                    classNames={{ base: "custom-input" }}
-                  />
-                </div>
-              </div>
-
-              <div className="date-container">
-                <DatePicker
-                  label="Data de Nascimento"
-                  variant="bordered"
-                  radius="lg"
-                  fullWidth
-                  value={formData.dataNascimento}
-                  onChange={(date) => handleInputChange("dataNascimento", date)}
-                  isInvalid={!!errors.dataNascimento}
-                  errorMessage={errors.dataNascimento}
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="select-container">
-                <Input
-                  type="text"
-                  className="select-cursos"
-                  label="Curso"
-                  variant="bordered"
-                  radius="lg"
-                  fullWidth
-                  onChange={(e) => handleInputChange("curso", e.target.value)}
-                  isInvalid={!!errors.curso}
-                  errorMessage={errors.curso}
-                >
-                </Input>
-              </div>
-
-              <div className="select-container">
-                <Select
-                  className="select-campus"
-                  label="Campus"
-                  variant="bordered"
-                  radius="lg"
-                  fullWidth
-                  selectedKeys={[formData.campus]}
-                  onChange={(e) => handleInputChange("campus", e.target.value)}
-                  isInvalid={!!errors.campus}
-                  errorMessage={errors.campus}
-                >
-                  {campus.map((campi) => (
-                    <SelectItem key={campi.valor}>{campi.label}</SelectItem>
-                  ))}
-                </Select>
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="cpf"
-                  value={formData.cpf}
-                  label="CPF"
-                  variant="bordered"
-                  radius="lg"
-                  type="text"
-                  placeholder="Digite seu CPF"
-                  onChange={(e) =>
-                    handleInputChange("cpf", formatCPF(e.target.value))
-                  }
-                  isInvalid={!!errors.cpf}
-                  errorMessage={errors.cpf}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="date-container">
-                <DatePicker
-                  label="Data de Ingresso"
-                  variant="bordered"
-                  radius="lg"
-                  fullWidth
-                  value={formData.dataIngresso}
-                  onChange={(date) => handleInputChange("dataIngresso", date)}
-                  isInvalid={!!errors.dataIngresso}
-                  errorMessage={errors.dataIngresso}
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="matricula"
-                  value={formData.matricula}
-                  label="Matrícula"
-                  variant="bordered"
-                  radius="lg"
-                  type="text"
-                  placeholder="Digite a sua matrícula"
-                  onChange={(e) =>
-                    handleInputChange("matricula", e.target.value)
-                  }
-                  isInvalid={!!errors.matricula}
-                  errorMessage={errors.matricula}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="email"
-                  value={formData.email}
-                  label="Email"
-                  variant="bordered"
-                  radius="lg"
-                  type="email"
-                  placeholder="Digite seu email @ufba.br"
-                  onChange={(e) => handleInputChange("email", e.target.value)}
-                  isInvalid={!!errors.email}
-                  errorMessage={errors.email}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="celular"
-                  value={formData.celular}
-                  label="Celular"
-                  variant="bordered"
-                  radius="lg"
-                  type="text"
-                  placeholder="Digite o seu celular"
-                  onChange={(e) => handleInputChange("celular", e.target.value)}
-                  isInvalid={!!errors.celular}
-                  errorMessage={errors.celular}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="senha"
-                  label="Senha"
-                  variant="bordered"
-                  radius="lg"
-                  type="password"
-                  placeholder="Digite sua senha"
-                  value={formData.senha}
-                  onChange={(e) => handleInputChange("senha", e.target.value)}
-                  isInvalid={!!errors.senha}
-                  errorMessage={errors.senha}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <div className="input-div">
-                <Input
-                  id="confirmarSenha"
-                  label="Repita sua senha"
-                  variant="bordered"
-                  radius="lg"
-                  type="password"
-                  placeholder="Digite sua senha novamente"
-                  value={formData.confirmarSenha}
-                  onChange={(e) =>
-                    handleInputChange("confirmarSenha", e.target.value)
-                  }
-                  isInvalid={!!errors.confirmarSenha}
-                  errorMessage={errors.confirmarSenha}
-                  fullWidth
-                  classNames={{ base: "custom-input" }}
-                />
-              </div>
-
-              <Button
-                type="submit"
-                radius="lg"
-                fullWidth
-                className="cadastro-button"
-                color="primary"
-                isLoading={isSubmitting}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Cadastrando..." : "Avançar"}
-              </Button>
-
-              <div className="copyright-text">
-                © {anoAtual} UFBA PROAE. Todos os direitos reservados.
-              </div>
-            </form>
-          </div>
-        </div>
+        </form>
       </div>
-    </div>
+    </section>
   );
 }
